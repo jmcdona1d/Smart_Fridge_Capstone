919c0f096721bb7e01f0c142a57164773323dc50
ELEC490-10 <james_mcd98@hotmail.com>
2020-10-27 16:29:10 -0400

    Updated camera testing to display 3 outputs at a time

--- objectDetection/cameraTest.py
+++ objectDetection/cameraTest.py
@@ -1,21 +1,30 @@
 import cv2
 print(cv2.__version__)
-cam = cv2.VideoCapture(0)
+cam1 = cv2.VideoCapture(0)
+cam2 = cv2.VideoCapture(1)
+cam3 = cv2.VideoCapture(2)
 
-cv2.namedWindow("test")
+cv2.namedWindow("cam1")
 
 while True:
 
-    ret, frame = cam.read()
-    if not ret:
-        print("failed to grab frame")
+    ret1, frame1 = cam1.read()
+    ret2, frame2 = cam2.read()
+    ret3, frame3 = cam3.read()
+
+    if not ret1 or not ret2 or not ret3:
+        print("failed to grab frames")
         break
-    cv2.imshow("test", frame)
+    cv2.imshow("cam1", frame)
+    cv2.imshow('cam2', frame2)
+    cv2.imshow('cam3', frame3)
 
     k = cv2.waitKey(1)
     if k%256 == 27:
         print("Escape hit, closing")
         break
 
-cam.release()
+cam1.release()
+cam2.release()
+cam3.release()
 cv2.destroyAllWindows()

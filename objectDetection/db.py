0637226199aa4513566f6cbb1d62177a6f04b26a
ELEC490-10 <james_mcd98@hotmail.com>
2021-04-03 19:48:35 -0400

    db + server

--- objectDetection/db.py
+++ objectDetection/db.py
@@ -5,14 +5,14 @@ import image_store
 dbconnection = "mongodb+srv://{}:{}@capstone.o2r0q.mongodb.net/test?retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE".format("user", "8nA4UGa1SwF2CeJg")
 database = MongoClient(dbconnection)
 
-fridge_content  = database.FridgeContents.user0 #table of items for "user0" - possibly this could be dynamic if we implement support for multi-users
+fridge_content  = database.FridgeContents.user1 #table of items for "user0" - possibly this could be dynamic if we implement support for multi-users
                   
 def upload_to_fridge( data ):
 
     global fridge_content
     
     fridge_content.drop()
-    fridge_content.insert_one({
+    fridge_content.insert_on(
        "data" : data
        })
 

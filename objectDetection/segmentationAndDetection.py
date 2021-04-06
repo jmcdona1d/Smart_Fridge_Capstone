a8cc8dff337e656e777f05cfb352a68d29097ee4
aadiyat.ahmad <aadiyat.ahmad@gmail.com>
2021-04-06 13:02:52 -0400

    refactor into function that takes cv2 img as input and returns annotation

--- objectDetection/segmentationAndDetection.py
+++ objectDetection/segmentationAndDetection.py
@@ -5,88 +5,78 @@ import numpy as np
 import tensorflow as tf
 
 #hyperparameters
-IMAGE_HEIGHT = 128
-SCORE_THRESHOLD = 0.99
+IMAGE_HEIGHT = 512
+SCORE_THRESHOLD = .98
 NUM_PROPOSALS = 2000
 
 def filter_predictions( predictions, rects, threshold):
     keep = np.where(np.amax(predictions,1) > threshold)
     return predictions[keep], rects[keep]
 
-def label_objects(img, label_indices, rects):
-    font = cv.FONT_HERSHEY_SIMPLEX
-    font_scale = 0.5
-    rect_colour = (255,0,0)
-    line_width = 3
-    text_spacing = 30
-    for i, label in enumerate(label_indices):
-        x, y, w, h = rects[i]
-        cv.rectangle(img, (x,y), (x+w, y+h), rect_colour, line_width)
-        cv.putText(img, class_ids[label], (x+text_spacing, y+text_spacing), font, font_scale, (0,0,0), thickness= 5)
-        cv.putText(img, class_ids[label], (x+text_spacing, y+text_spacing), font, font_scale, (255,255,255), thickness=1)
 
-    cv.imshow('image', img)
-    cv.waitKey(0)
-
-# Load saved model
-model_path = '../models/freiburg_mobilenetv2_model'
-saved_model = tf.keras.models.load_model(model_path)
-saved_model = tf.keras.Sequential([saved_model, 
-                                         tf.keras.layers.Softmax()])
+def rcnnDetect(img):
+    # Load saved model
+    model_path = '../models/freiburg_mobilenetv2_model'
+    saved_model = tf.keras.models.load_model(model_path)
+    saved_model = tf.keras.Sequential([saved_model, 
+                                            tf.keras.layers.Softmax()])
 
 
-# read class ids
-f = open('../objectDetection/classid.txt', 'r')
-class_ids = np.array(f.read().splitlines())
+    # read class ids
+    f = open('../objectDetection/classid.txt', 'r')
+    class_ids = np.array(f.read().splitlines())
 
-# Read image
-img_path = './test_images/lightingfix1.jpg'
-img = cv.imread(img_path)
+    # resize image
+    resize_factor = IMAGE_HEIGHT/img.shape[0]
+    new_width = int(img.shape[1]*resize_factor)
 
+    resized = cv.resize(img, (new_width, IMAGE_HEIGHT))
 
+    # create selective search segmentation object using default params            
+    ss = cv.ximgproc.segmentation.createSelectiveSearchSegmentation()
 
-# resize image
-resize_factor = IMAGE_HEIGHT/img.shape[0]
-new_width = int(img.shape[1]*resize_factor)
+    ss.setBaseImage(resized)
+    ss.switchToSelectiveSearchFast()
 
-resized = cv.resize(img, (new_width, IMAGE_HEIGHT))
-
-# create selective search segmentation object using default params            
-ss = cv.ximgproc.segmentation.createSelectiveSearchSegmentation()
-
-ss.setBaseImage(resized)
-ss.switchToSelectiveSearchFast()
-
-# Get region proposals in decreasing order of 'objectness'
-rects = ss.process()
+    # Get region proposals in decreasing order of 'objectness'
+    rects = ss.process()
 
 
-# Crop and collect region proposals to create a batch of images
-rects = rects/resize_factor # Get coordinates for original image size
-rects = rects.astype(int)
+    # Crop and collect region proposals to create a batch of images
+    rects = rects/resize_factor # Get coordinates for original image size
+    rects = rects.astype(int)
 
-batch = np.empty([rects.shape[0], 256, 256,3])
-predictions = np.empty([rects.shape[0], 25])
-# Find a better way to do this
-for i, rect in enumerate(rects):
-    if(i<NUM_PROPOSALS):
-        x,y,w,h = rect # bounding box of region proposal
-        if( w ==0 or h == 0):
-            continue
+    pred_size = NUM_PROPOSALS if (rects.shape[0] > NUM_PROPOSALS) else rects.shape[0]
 
-        region = img[y:y+h, x:x+w] # crop image according to bounding box
-        warped_region = cv.resize(region, (256, 256)) # warp region to input size of CNN
-        region_batch = warped_region[np.newaxis, :] # input has to be a batch
-        pred = saved_model.predict(region_batch)
-        predictions[i] = pred[0]
+    predictions = np.empty([pred_size, class_ids.size])
+    # Find a better way to do this
+    for i, rect in enumerate(rects):
+        if(i<NUM_PROPOSALS):
+            x,y,w,h = rect # bounding box of region proposal
+            if( w ==0 or h == 0):
+                continue
 
-   
-# predictions = saved_model.predict(batch)
+            region = img[y:y+h, x:x+w] # crop image according to bounding box
+            warped_region = cv.resize(region, (256, 256)) # warp region to input size of CNN
+            region_batch = warped_region[np.newaxis, :] # input has to be a batch
+            pred = saved_model.predict(region_batch)
+            predictions[i] = pred[0]
 
-filtered_predictions, filtered_rects = filter_predictions(predictions, rects, SCORE_THRESHOLD)
-label_indices = np.argmax(filtered_predictions, axis=1)
+    
 
-if(label_indices.size == 0):
-    print("No objects found.")
-print(class_ids[label_indices])
-# label_objects(img, label_indices, filtered_rects)
+    filtered_predictions, filtered_rects = filter_predictions(predictions, rects, SCORE_THRESHOLD)
+    label_indices = np.argmax(filtered_predictions, axis=1)
+    scores = np.max(filtered_predictions, axis=1)
+
+
+    annotation = {
+        "items": filtered_predictions.shape[0],
+        "xmin": [filtered_rects[i,0].item() for i in range(filtered_predictions.shape[0])],
+        "ymin": [filtered_rects[i,1].item() for i in range(filtered_predictions.shape[0])],
+        "xmax": [(filtered_rects[i,0].item() + filtered_rects[i][2].item()) for i in range(filtered_predictions.shape[0])],
+        "ymax": [(filtered_rects[i,1].item() + filtered_rects[i][3].item()) for i in range(filtered_predictions.shape[0])],
+        "classes": [label_indices[i].item() for i in range(filtered_predictions.shape[0])],
+        "classes_text": [class_ids[label_indices[i]] for i in range(filtered_predictions.shape[0])],
+        "softmax": [scores[i].item() for i in range(filtered_predictions.shape[0])]
+    }
+    return annotation

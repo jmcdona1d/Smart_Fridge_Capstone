c86f1ac6845b13e6197c4f0d1c8b7c7e2b352ab2
James McDonald <james_mcd98@hotmail.com>
2021-04-05 22:33:18 -0400

    fixed merging

--- objectDetection/db.py
+++ objectDetection/db.py
@@ -5,7 +5,7 @@ import image_store
 dbconnection = "mongodb+srv://{}:{}@capstone.o2r0q.mongodb.net/test?retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE".format("user", "8nA4UGa1SwF2CeJg")
 database = MongoClient(dbconnection)
 
-fridge_content  = database.FridgeContents.user1 #table of items for "user0" - possibly this could be dynamic if we implement support for multi-users
+fridge_content  = database.FridgeContents.user0  #table of items for "user0" - possibly this could be dynamic if we implement support for multi-users
                   
 def upload_to_fridge( data ):
 
@@ -20,7 +20,6 @@ def merge_old_new( data, images ):
 
     result = {}
     class_names = {}
-    result["total_items"] = data["items"]
     result["items"] = []
     time_now = datetime.today()
 
@@ -30,23 +29,23 @@ def merge_old_new( data, images ):
 
         for i in range(data_set["items"]):
 
-            if item["class_text"] in class_names:
+            if data_set["classes_text"][i] in class_names:
                 continue
 
             item = {}
-            item["xmin"] = data["xmin"][i]
-            item["xmax"] = data["xmax"][i]
-            item["ymin"] = data["ymin"][i]
-            item["ymax"] = data["ymax"][i]
-            item["class"] = data["classes"][i]
-            item["class_text"] = data["classes_text"][i]
-            item["softmax"] = data["softmax"][i]
+            item["xmin"] = data_set["xmin"][i]
+            item["xmax"] = data_set["xmax"][i]
+            item["ymin"] = data_set["ymin"][i]
+            item["ymax"] = data_set["ymax"][i]
+            item["class"] = data_set["classes"][i]
+            item["class_text"] = data_set["classes_text"][i]
+            item["softmax"] = data_set["softmax"][i]
             item["timestamp_added"] = time_now
             # Fix this with cropped images
             item["image_url"] = "https://res.cloudinary.com/dcead5pak/image/upload/v1615077687/e99jsqiiixmii5e9eeep.jpg"
             result["items"].append(item)
-            class_names[data["classes_text"]] = True
-
+            class_names[data_set["classes_text"][i]] = True
+    result["total_items"] = len(result["items"])
     #Upload images to cloud
     urls = []
     for img in images:
